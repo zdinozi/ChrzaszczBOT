e7e3f2972242523d8c241509ed0d4caa894aa103
Wiktor <zdi724@gmail.com>
2024-10-13 21:14:42 +0200

    initial configuration

---
+++ main.py
@@ -0,0 +1,26 @@
+# This example requires the 'message_content' intent.
+
+import discord
+
+def getToken():
+    token = open("token.txt", "r")
+    return token.read()
+    
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(getToken())
